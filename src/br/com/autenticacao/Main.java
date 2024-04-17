dd1fbb1517749b7376ad755b2c5e59e1e83a8d3d
Janah Kuehl <janahs2@hotmail.com>
2024-04-16 21:53:57 -0300

    :sparkles: feat(usuario): CRUD Usuario

--- src/br/com/autenticacao/Main.java
+++ src/br/com/autenticacao/Main.java
@@ -1,10 +1,21 @@
 package br.com.autenticacao;
 
+import br.com.autenticacao.controller.UsuarioController;
+import br.com.autenticacao.model.Usuario;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		UsuarioController controller = new UsuarioController();
+		
+		Usuario usuario = new Usuario();
+		usuario.setNome("Anthony");
+		usuario.setEmail("anthony@rhcp.com");
+		usuario.setSenha("Ant123");
+		usuario.setIsAtivo(true);
+		
+		controller.cadastrar(usuario);
 	}
 
 }
